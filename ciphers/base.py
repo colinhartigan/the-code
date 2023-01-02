8960e87caebf39603d05653f15c46b2a3787a554
colin <colinjoe9@gmail.com>
2023-01-01 22:36:33 -0500

    ciphers

---
+++ ciphers/base.py
@@ -0,0 +1,95 @@
+base = [
+	["A", "B", "C", "D", "E", "F",],
+	["G", "H", "I", "J", "K", "L",],
+	["M", "N", "O", "P", "Q", "R",],
+	[["S", "Z"], "T", "U", ["V", "W"], "X", "Y"],
+]
+
+def decode(text):
+
+	# convert text to uppercase for consistency
+	text = text.upper()
+
+	# split text into words
+	split = text.split()
+
+	output = []
+
+	for word in split:
+
+		built = []
+
+		# iterate through each number in the word
+		# instead of going a number pair at a time, we instead go character by character to easily find any punctuation
+		i = 0
+		while i < len(word):
+			char = word[i]
+
+			# if the character is not a number, add it to the done list
+			if not char.isnumeric():
+				built.append(char)
+				i += 1
+				continue
+			
+			else:
+				# separate the number pair into the indicies
+				i1 = int(char)
+				i2 = int(word[i+1])
+
+				# get the value at the index
+				value = base[i1 - 1][i2 - 1]
+
+				# for multi-value entries, just use the first one
+				if isinstance(value, list):
+					value = value[0]
+				
+				built.append(value)
+
+				i += 2
+				continue
+		
+		output.append("".join(built))
+
+	return " ".join(output) 
+
+
+def encode(text):
+
+	output = []
+
+	text = text.upper()
+
+	split = text.split()
+
+	for word in split:
+
+		built = []
+
+		for char in word:
+
+			if not char.isalpha():
+				built.append(char)
+				continue
+			
+			else:
+				# find the index of the character
+				enc = ""
+				for i1, row in enumerate(base):
+					for i2, value in enumerate(row):
+						
+						if isinstance(value, list):
+							if char in value:
+								enc = str(i1+1) + str(i2+1)
+						elif char == value:
+							enc = str(i1+1) + str(i2+1)
+
+				built.append(enc)
+		
+		output.append("".join(built))
+	
+	return " ".join(output)
+
+
+# a = encode("Hello World!")
+
+# print(decode(a))
---
+++ ciphers/v2.py
@@ -0,0 +1,104 @@
+import base 
+
+def factorize(s):
+	# find factors
+	factors = [i for i in range(1, s+1) if s % i == 0]
+
+	# get middle 2 factors
+	factors = factors[len(factors)//2-1:len(factors)//2+1]
+
+	return factors
+
+def diagonalize(factors):
+	# generates the path for the diagonal traversal
+	rows = factors[0]
+	cols = factors[1]
+
+	order = []
+
+	for diagonal_num in range(1, (sum(factors))):
+
+		# calcualte which column to start at
+		# when diagonal_num is less than the # of rows, start at 0
+		start_col = max(0, diagonal_num - rows)
+
+		# caluclate how many numbers there will be in the diagonal
+		count = min(diagonal_num, (cols - start_col), rows)
+
+		# find the diagonal coordinates and add them to the output
+		for j in range(0, count):
+			order.append((min(rows, diagonal_num) - j - 1, start_col + j))
+
+	return order 
+
+
+def decode(text):
+
+	nums = [i for i in text]
+	s = len(nums)
+
+	factors = factorize(s)
+
+	new = []
+
+	rows = factors[0]
+	cols = factors[1]
+	
+	# split up the array into the new matrix form
+	for i in range(rows):
+		new.append(nums[i*cols:i*cols+cols])
+
+	reformatted = []
+
+	coords = diagonalize(factors)
+
+	for coord in coords:
+		reformatted.append(str(new[coord[0]][coord[1]]))
+
+	# use regular decoding for the rest
+	return base.decode("".join(reformatted))
+
+
+def encode(text):
+
+	# use regular encoding
+	# reverse it to make it easier to build the matrix
+	encoded = list(base.encode(text))[::-1]
+
+	s = len(encoded)
+
+	factors = factorize(s)
+
+	rows = factors[0]
+	cols = factors[1]
+
+	# fill array with placeholders
+	new = [
+		[0 for i in range(cols)] for j in range(rows)
+	]
+
+	# find diagonal path coords
+	coords = diagonalize(factors)
+
+	# fill in the table
+	for coord in coords:
+		new[coord[0]][coord[1]] = encoded.pop()
+
+	# compress it to one line and join it
+	new = ["".join(i) for i in new]
+
+	return str("".join(new))
+			
+
+
+
+
+
+# e = encode("hello")
+# print(e)
+# print(decode(e))
+
+e = encode("upon further inspection, it looks like we can just straight up include punctuation and it works fine! it makes it even more unrecognizable because the punctuation locations get scrambled. my only woe is the fact that multiple spaces might appear next to eachother.")
+print(e)
+
+print(decode(e))
